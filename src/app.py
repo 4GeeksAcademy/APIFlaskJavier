efb9fc11358e9107a102ef855eb1f8ebbac40301
JavierML-git <mjavivi673@gmail.com>
2025-04-07 11:35:02 +0000

    Javier

--- src/app.py
+++ src/app.py
@@ -15,6 +15,25 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7,13,22]
+})
+
+jackson_family.add_member({
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10,14,3]
+})
+
+jackson_family.add_member({
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+})
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -35,8 +54,22 @@ def handle_hello():
         "family": members
     }
 
-
     return jsonify(response_body), 200
+
+@app.route('/members', methods=['POST'])
+def add_family():
+    request_body = request.get_json()
+    family = jackson_family.add_member(request_body)
+    return jsonify(family), 202
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def remove_family(id):
+    member = jackson_family.get_member(id)
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message" : "Member deleted"}),205
+    else:
+        return jsonify({"message" : "Member not deleted"}),406
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,31 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member["id"] = self._generateId()
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"]==id:
+                self._members.remove(member)
+                print(f"User {member["name"]} se ha eliminado")
+                return{
+                    "Done": "true"
+                }
+            return{
+                "error": f"No se encontro ningun member con ese ID {id}"
+            }
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self.members:
+            if member["id"] == id:
+                return member
+        return{
+            "error":"Not exist"
+        }
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
